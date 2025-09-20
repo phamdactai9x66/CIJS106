003b824bbc0037d408dabd696782ff2de8ef8c55
tai.pham <tai15122003311@gmail.com>
2025-09-20 21:34:52 +0700

    draw UI form modal craete task

--- src/components/ModalCreateCart.jsx
+++ src/components/ModalCreateCart.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { taskStatus } from "../constant";
 
 const ModalCreateCart = (props) => {
   return (
     <div className="modalCreateCart">
       <div className="iconClose" onClick={props.onCloseModal}>
         X
+      </div>
+
+      <h1>Save Task</h1>
+
+      <div className="form">
+        {/* field Title */}
+        <div className="input">
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" />
+        </div>
+
+        <div className="input">
+          <label htmlFor="description">Description:</label>
+          <input type="text" id="description" />
+        </div>
+
+        {/* list dropdown */}
+
+        <select name="status" id="">
+          {taskStatus.map((e) => {
+            return (
+              <option value={e.statusId} key={e.statusId}>
+                {e.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <div className="btn">
+        <button>Cancel</button>
+        <button>Save</button>
       </div>
     </div>
   );
