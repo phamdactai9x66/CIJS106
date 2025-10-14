7b4fcf19aaf5529a1cff5e4643176db7ed6527eb
tai.pham <tai15122003311@gmail.com>
2025-10-14 22:02:55 +0700

    implement API

--- src/components/GeminiAPI.jsx
+++ src/components/GeminiAPI.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyDm8iAOSoBQKLrzvw6Xsd8ouSDgrP97CQg");
+
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+// const fakeResponse = new Promise((res, rej) => {
+//   try {
+//     setTimeout(() => {
+//       res("AI: Hello World");
+//     }, 3000);
+//   } catch (error) {
+//     rej(error);
+//   }
+// });
 
 const GeminiAPI = (props) => {
-  console.log(props);
+  const [listConversation, setListConversation] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const inputRef = useRef("");
+
+  const onSubmit = async () => {
+    const inputValue = inputRef.current.value;
+
+    if (!inputValue) return;
+
+    // logic generate content
+
+    setLoading(true);
+
+    const resAPI = await model.generateContent(inputValue);
+
+    const textRes = resAPI.response.text();
+
+    setListConversation((preValue) => {
+      return [
+        ...preValue,
+        {
+          id: Date.now() + "_user",
+          type: "user",
+          message: inputValue,
+        },
+        {
+          id: Date.now() + "_bot",
+          type: "bot",
+          message: textRes,
+        },
+      ];
+    });
+
+    setLoading(false);
+  };
 
   return (
     <div className="geminiApiContainer">
@@ -17,19 +69,34 @@ const GeminiAPI = (props) => {
         {props.modalGeminiAPI ? (
           <div className="boxGeMiniAPI">
             <div className="conversation">
-              <div className="userType">user: abc...</div>
-              <div className="botType">bot: aawdawd</div>
+              {listConversation.map((item) => {
+                if (item.type == "user") {
+                  return (
+                    <div className="userType" key={item.id}>
+                      <span className="bold">user</span>: {item.message}
+                    </div>
+                  );
+                }
+                return (
+                  <div className="botType" key={item.id}>
+                    <span className="bold">bot</span>: {item.message}
+                  </div>
+                );
+              })}
 
-              <hr />
+              {loading ? <div>Loading...</div> : ""}
             </div>
 
             <div className="requestAI">
               <input
                 type="text"
                 placeholder="Typing..."
+                ref={inputRef}
                 className="w-full border"
               />
-              <button className="pointer">Send</button>
+              <button className="pointer" onClick={onSubmit}>
+                Send
+              </button>
             </div>
           </div>
         ) : (
