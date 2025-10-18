246c1b31db514afb4576554a92df5b9d760eef3d
tai.pham <tai15122003311@gmail.com>
2025-10-18 21:51:23 +0700

    create a fake conflict

--- src/components/GeminiAPI.jsx
+++ src/components/GeminiAPI.jsx
@@ -2,19 +2,9 @@ import React, { useState, useRef } from "react";
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyDm8iAOSoBQKLrzvw6Xsd8ouSDgrP97CQg");
+const genAI = new GoogleGenerativeAI("zvw6Xsd8ouSDgrP97CQg");
 
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
-// const fakeResponse = new Promise((res, rej) => {
-//   try {
-//     setTimeout(() => {
-//       res("AI: Hello World");
-//     }, 3000);
-//   } catch (error) {
-//     rej(error);
-//   }
-// });
 
 const GeminiAPI = (props) => {
   const [listConversation, setListConversation] = useState([]);
