26c3f37ca7513d042363fadce8d2351fde5d89d8
tai.pham <tai15122003311@gmail.com>
2025-10-25 19:54:50 +0700

    update username

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
   return (
     <div>
       <button onClick={() => props.handleLogin()}>
-        {props.isLogin ? "Welcome back: Tai Pham" : "Login"}
+        {props.isLogin ? "Welcome back: Quang Tran" : "Login"}
       </button>
       |<Link to="/list_task">List Task</Link>|
       <Link to="/list_task/1">Task Detail</Link>
